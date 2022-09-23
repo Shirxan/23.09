44cdf3300c5c7764ae56b0b3feff02b609971412
abilov.w <109854878+Shirxan@users.noreply.github.com>
2022-09-23 11:40:13 +0300

    Add files via upload

---
+++ 22.09/scrip.js
@@ -0,0 +1,6 @@
+let inp = document.getElementById('inp')
+let show = document.getElementById('show')
+function chek(){
+    show.innerHTML = inp.value === 4 ? ``: `K&m5`;
+    show.innerHTML = inp.value === 6 ? ``: `b!k52K&8`;
+}
